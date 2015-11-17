42231896eb57ae0435e362bc76fb6368a8552567
jbreckmckye <jimmy@breck-mckye.com>
2015-11-17 20:51:49 +0000

    Make buildable

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var recipe = require('./recipe');
-var speak = require('./speak');
 var ko = require('knockout');
 var voiceListen = require('./voiceListen');
 var lodash = require('lodash');
