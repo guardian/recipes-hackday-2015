fb19d736228a1332f5deb0bdb817cc17c366b6d0
jbreckmckye <jimmy@breck-mckye.com>
2015-11-17 22:50:08 +0000

    Voila

--- javascripts/recipe.js
+++ javascripts/recipe.js
@@ -5,5 +5,6 @@ module.exports = [
 	"Slice your bread. In this example, I’m using a free range, open source, organic San Francisco sourdough).",
 	"Remove the half toasted bread and turn it, so that the untoasted side is facing upwards.",
 	"Place 100g of cheese on each slice, and return to the grill for 3-5 minutes.",
-	"Remove your cheese on toast from the grill, and add splashes of Lea & Perrins sauce and a sprinkle of popping candy."
+	"Remove your cheese on toast from the grill, and add splashes of Lea & Perrins sauce and a sprinkle of popping candy.",
+	"Serve and enjoy"
 ];
